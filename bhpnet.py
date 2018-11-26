19a53a1201c3aa4542f2b97b1df80bc6d5771ceb
grassSquare <862715840@qq.com>
2018-11-26 17:26:47 +0800

    complete , but fail to run

--- bhpnet.py
+++ bhpnet.py
@@ -54,9 +54,93 @@ def client_sender(buffer):
                     break
             print(response)
 
-            buffer
+            buffer = input("")
+            buffer += "\n"
 
+            client.send(buffer)
 
+    except:
+        print("[*]Exception! Exiting")
+        client.close
+
+def server_loop():
+    global target
+
+    #
+    if not len(target):
+        target = "0.0.0.0"
+
+    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    server.bind((target,port))
+
+    server.listen(5)
+
+    while True:
+        client_socket,addr = server.accept()
+
+        #
+        client_thread = threading.Thread(target= client_handler,args=(client_socket,))
+        client_thread.start()
+
+def run_command(command):
+
+    command =command.rstrip()
+
+    try:
+        output = subprocess.check_output(command,stderr=subprocess.STDOUT,shell=True)
+    except:
+        output = "Failed toexecute command.\r\n"
+
+    return output
+
+def client_handler(client_socket):
+    global upload
+    global execute
+    global command
+
+    if len(upload_destination):
+
+        #
+        file_buffer = ""
+
+        #
+
+        while True:
+            data = client_socket.recv(1024)
+
+            if not data:
+                break
+            else:
+                file_buffer += data
+
+        try :
+            file_descriptor = open(upload_destination,"wb")
+            file_descriptor.write(file_buffer)
+            file_descriptor.close()
+
+            client_socket.send("Successfully save file to %s\r\n" %upload_destination)
+        except:
+            client_socket.send("Failed to  save file to %s\r\n" %upload_destination)
+
+    if len(execute):
+
+        #
+        output = run_command(execute)
+
+        client_socket.send(output)
+
+    #
+    if command :
+        while True:
+            #
+            client_socket.send("<BHP:#> ")
+
+            #
+            cmd_buffer = ""
+            while "\n" not in cmd_buffer:
+                cmd_buffer += client_socket.recv(1024)
+                response = run_command(cmd_buffer)
+                client_socket.send(response)
 
 def main():
     global listen
