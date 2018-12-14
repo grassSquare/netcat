f8c58547dca41b1d576239da72ff8c3edadc421b
grassSquare <862715840@qq.com>
2018-12-14 13:30:00 +0800

    服务端改进

--- flashSocketServer.py
+++ flashSocketServer.py
@@ -1,19 +1,28 @@
 from tkinter import *
 import socket
-import easygui as g
+
+def msgbox(msg:str)->Tk:
+    msgbox = Tk()
+    Label1 = Label(msgbox, text=msg)
+    Label1.gird()
+    msgbox.mainloop()
 
 def startLink():
-    g.msgbox("succsee")
-
+    port = int(Text1.get())
+    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    server.bind(("localhost",port))
+    server.listen(5)
+    while True:
+        pass
 if __name__ == "__main__":
     tk = Tk()
 
     Label1 = Label(tk,text="用哪个端口收发？：")
     Text1 = Entry(tk)
-    Button1 = Button(tk,text="Start Link",command=startLink())
+    Button1 = Button(tk,text="Start Link",command=startLink)
 
-    Label1.pack()
-    Text1.pack()
-    Button1.pack()
+    Label1.grid()
+    Text1.grid()
+    Button1.grid()
 
     tk.mainloop()
